2700ebaab80b92871ca4df56ba4310d1e5bb3bf6
Devon Moss <dvnmss@gmail.com>
2013-05-11 17:41:11 -0600

    added gameday facet and restructured maven project

---
+++ historical-data-fetcher/src/main/java/Fetcher.java
@@ -0,0 +1,12 @@
+/**
+ * User: devon
+ * Date: 5/9/13
+ * Time: 9:50 AM
+ */
+public class Fetcher {
+    private static final String BASE_URL = "gd2.mlb.com/components/game/mlb/";
+    private static final String YEAR = "year_"; // year_yyyy
+    private static final String MONTH = "month_"; // month_mm
+    private static final String DAY = "day_"; // day_dd
+
+}
---
+++ historical-data-fetcher/src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * User: devon
+ * Date: 5/9/13
+ * Time: 9:48 AM
+ */
+public class Main {
+    public static void main(String[] args) {
+        // file locations
+
+    }
+}
